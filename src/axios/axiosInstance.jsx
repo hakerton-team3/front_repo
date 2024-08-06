a7fa5b863086129b220227f436dbd641daff62fe
seongbeom <ssdk128@naver.com>
2024-08-06 14:00:25 +0900

    url변경

--- src/axios/axiosInstance.jsx
+++ src/axios/axiosInstance.jsx
@@ -2,9 +2,9 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL:   'http://ec2-43-201-61-252.ap-northeast-2.compute.amazonaws.com:8080/api/v1',
-  // 'https://api.smartcheers.site/api/v1', // 배포시 url
-  
+  baseURL:   'https://api.smartcheers.site/api/v1', 
+
+  // 'http://ec2-43-201-61-252.ap-northeast-2.compute.amazonaws.com:8080/api/v1',
   // 로컬에서 사용할시 url
 
       
