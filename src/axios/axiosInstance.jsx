9f749267ebc7c330cfc16fda91569b68ec806072
seongbeom <ssdk128@naver.com>
2024-08-06 22:56:27 +0900

    d

--- src/axios/axiosInstance.jsx
+++ src/axios/axiosInstance.jsx
@@ -2,7 +2,11 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL:     'https://api.smartcheers.site/api/v1',
+  // baseURL: "http://ec2-43-201-61-252.ap-northeast-2.compute.amazonaws.com:8080/api/v1",
+  
+  baseURL: 'https://api.smartcheers.site/api/v1',
+
+ 
   timeout: 10000,
   headers: {    
     'Content-Type': 'application/json',
